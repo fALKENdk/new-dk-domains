409c0672bbd2df28595783cbf5dbf29ee625a88c
Kasper Friis Christensen <kasper@friischristensen.com>
2018-04-12 00:04:14 +0100

    Loop files in zip

--- get-domains.py
+++ get-domains.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 def read_zip_file(filepath):
   nrd = zipfile.ZipFile(filepath)
-  df = pd.read_csv(nrd.open('2018-04-10.txt'), names=['domain'])
-  print(df[df['domain'].str.endswith(".dk")])
- 
-read_zip_file("nrd")
+  for domainlist in nrd.infolist():
+    df = pd.read_csv(nrd.open(domainlist), names=['domain'])
+    print(df[df['domain'].str.endswith('.dk')])
+
+read_zip_file('nrd')
