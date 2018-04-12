690ffa2dca2ea35ab3e9dc7e76fc526f464ebd10
Kasper Friis Christensen <kasper@friischristensen.com>
2018-04-12 13:51:33 +0100

    Save domains to md file

--- get-domains.py
+++ get-domains.py
@@ -1,10 +1,20 @@
-import zipfile
+import zipfile as zf
 import pandas as pd
+import pytablewriter as ptw
 
-def read_zip_file(filepath):
-  nrd = zipfile.ZipFile(filepath)
+def zipToMarkdown(filepath):
+  nrd = zf.ZipFile(filepath)
   for domainlist in nrd.infolist():
     df = pd.read_csv(nrd.open(domainlist), names=['domain'])
-    print(df[df['domain'].str.endswith('.dk')])
+    danish = df[df['domain'].str.endswith('.dk')]
+    
+    md_headers = ['domain']
+    writer = ptw.MarkdownTableWriter()
+    writer.header_list = list(md_headers)
+    writer.value_matrix = danish.values.tolist()
 
-read_zip_file('nrd')
+    with open("domain-list.md", "w") as f:
+      writer.stream = f
+      writer.write_table()
+
+zipToMarkdown('nrd')
