8833e3c124c42c16f2f19b05a9742faeed1dd73c
Kasper Friis Christensen <kasper@friischristensen.com>
2018-04-12 23:50:38 +0100

    Download domain zipfile and generate md file

--- get-domains.py
+++ get-domains.py
@@ -1,20 +1,30 @@
 import zipfile as zf
 import pandas as pd
-import pytablewriter as ptw
+import pytablewriter
+import datetime
+import requests
 
-def zipToMarkdown(filepath):
-  nrd = zf.ZipFile(filepath)
-  for domainlist in nrd.infolist():
-    df = pd.read_csv(nrd.open(domainlist), names=['domain'])
-    danish = df[df['domain'].str.endswith('.dk')]
-    
-    md_headers = ['domain']
-    writer = ptw.MarkdownTableWriter()
-    writer.header_list = list(md_headers)
-    writer.value_matrix = danish.values.tolist()
+yesterday = str(datetime.date.fromordinal(datetime.date.today().toordinal()-1))
+filename = yesterday
+zipfile = ''.join([filename, ".zip"])
+zipurl = ''.join(["https://whoisds.com/whois-database/newly-registered-domains/", zipfile, "/nrd"])
+mdfile = ''.join([filename, ".md"])
+mdpath = ''.join(["./domains/", mdfile])
 
-    with open("domain-list.md", "w") as f:
-      writer.stream = f
-      writer.write_table()
+file = requests.get(zipurl)
+with open(zipfile, 'wb') as f:
+  f.write(file.content)
 
-zipToMarkdown('nrd')
+domain_file = zf.ZipFile(zipfile)
+for domainlist in domain_file.infolist():
+  df = pd.read_csv(domain_file.open(domainlist), names=['domain'])
+  danish = df[df['domain'].str.endswith('.dk')]
+  
+  md_headers = ['domain']
+  writer = pytablewriter.MarkdownTableWriter()
+  writer.header_list = list(md_headers)
+  writer.value_matrix = danish.values.tolist()
+
+  with open(mdpath, "w") as f:
+    writer.stream = f
+    writer.write_table()
