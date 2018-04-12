d44e7b460080022959757b9631cb5e7a440e5bb0
Kasper Friis Christensen <kasper@friischristensen.com>
2018-04-13 00:07:42 +0100

    Delete zipfile after createing md file

--- get-domains.py
+++ get-domains.py
@@ -1,8 +1,9 @@
-import zipfile as zf
+import datetime
+import os
 import pandas as pd
 import pytablewriter
-import datetime
 import requests
+import zipfile as zf
 
 yesterday = str(datetime.date.fromordinal(datetime.date.today().toordinal()-1))
 filename = yesterday
@@ -11,20 +12,26 @@ zipurl = ''.join(["https://whoisds.com/whois-database/newly-registered-domains/"
 mdfile = ''.join([filename, ".md"])
 mdpath = ''.join(["./domains/", mdfile])
 
+# Download zipfile
 file = requests.get(zipurl)
 with open(zipfile, 'wb') as f:
   f.write(file.content)
 
-domain_file = zf.ZipFile(zipfile)
-for domainlist in domain_file.infolist():
-  df = pd.read_csv(domain_file.open(domainlist), names=['domain'])
+# Unzip
+domainfile = zf.ZipFile(zipfile)
+# Check for dk domains in the files
+for domainlist in domainfile.infolist():
+  df = pd.read_csv(domainfile.open(domainlist), names=['domain'])
   danish = df[df['domain'].str.endswith('.dk')]
   
+  # Create md file
   md_headers = ['domain']
   writer = pytablewriter.MarkdownTableWriter()
   writer.header_list = list(md_headers)
   writer.value_matrix = danish.values.tolist()
-
   with open(mdpath, "w") as f:
     writer.stream = f
     writer.write_table()
+
+# Delete zipfile
+os.remove(zipfile)
